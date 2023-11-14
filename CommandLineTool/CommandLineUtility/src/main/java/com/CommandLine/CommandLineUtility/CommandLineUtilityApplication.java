ec919ea35e900713bf8c307f63d1780e6b1c70b7
Harry <shaikmohammadibrahim311@gmail.com>
2023-11-14 14:25:19 +0530

    no message

---
+++ CommandLineTool/CommandLineUtility/src/main/java/com/CommandLine/CommandLineUtility/CommandLineUtilityApplication.java
@@ -0,0 +1,13 @@
+package com.CommandLine.CommandLineUtility;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommandLineUtilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommandLineUtilityApplication.class, args);
+	}
+
+}
---
+++ CommandLineTool/CommandLineUtility/src/test/java/com/CommandLine/CommandLineUtility/CommandLineUtilityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CommandLine.CommandLineUtility;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommandLineUtilityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
